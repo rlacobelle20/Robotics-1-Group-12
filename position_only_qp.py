7abb5ebe0ef682ecaa95bfa609e0a4f2e0c5bc83
Rachel Lacobelle <rlacobelle02@gmail.com>
2023-11-03 16:39:03 -0400

    added lambda and fwdkin

--- position_only_qp.py
+++ position_only_qp.py
@@ -9,8 +9,10 @@ inputs: robot, q0, P0Td, epsilon_p, q_prime_min, q_prime_max, N
 output: q_lambda, lambda_, P0T_lambda, R0T_lambda
 """
 
-def qpPathGen_positionOnly(robot, q0, P0Td, epsilon_p, q_prime_min, q_prime_max, N)
+def qpPathGen_positionOnly(robot, q0, P0Td, epsilon_p, q_prime_min, q_prime_max, N):
     n = len(q0)
-    lambda_num = np.array()
-    lambda_ = 0:1/(N):1
+    lambda_= np.arange(0,1,1/N) # creates an array 0-1 in increments of 1/N
+    
+    [R0T0,P0T0] = fwdkin(robot,q0)
+    
     
