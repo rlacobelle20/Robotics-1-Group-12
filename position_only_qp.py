e3c05d133060f11017136640d5f5c230a579ac5f
Rachel Lacobelle <rlacobelle02@gmail.com>
2023-11-03 16:54:42 -0400

    finds G position

--- position_only_qp.py
+++ position_only_qp.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import scipy
+import math
 
 """
 translation of MATLAB code from hw into python
@@ -15,4 +16,12 @@ def qpPathGen_positionOnly(robot, q0, P0Td, epsilon_p, q_prime_min, q_prime_max,
     
     [R0T0,P0T0] = fwdkin(robot,q0)
     
-    
+def getqp_G_positionOnly(dq, J, vp, ep):
+    n = len(dq)
+    G1 = np.dot( ( np.array(J, np.zeros((3,1))) ).T,np.array( J, np.zeros((3,1)) ) )
+    G2 = np.dot( (np.array([np.zeros((3,n)), np.zeros((3,1))], [np.zeros((3,n)), vp])).T,  np.array([np.zeros((3,n)), np.zeros((3,1))], [np.zeros((3,n)), vp]))
+    G3 = -2 * np.dot( (np.array(J,np.zeros((3,1)))).T, np.array(np.zeros((3,n)), vp) )
+    G3 = (G3 + G3.T) / 2
+    G4 = np.dot( (np.array([np.zeros((1,n)), 0], [np.zeros((1,n)), math.sqrt(ep)])).T, np.array([np.zeros((1,n)), 0], [np.zeros((1,n)), math.sqrt(ep)]) )
+    G = 2 * (G1+G2+G3+G4)
+    return G
