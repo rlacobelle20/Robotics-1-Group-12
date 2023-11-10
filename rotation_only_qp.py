9aef4fc0d6ac75e5df87668e6dcce88a7548c83c
Rachel Lacobelle <rlacobelle02@gmail.com>
2023-11-10 14:47:07 -0500

    started loop

--- rotation_only_qp.py
+++ rotation_only_qp.py
@@ -21,5 +21,14 @@ def qpPathGen_rotationOnly(robot,q0,R0Td,epsilon_r,q_prime_min,q_prime_max,N):
     # compute path in task space
     ER0 = np.dot(R0Td,R0Td.T) # ER0 = R0T0*R0Td'
     temp = vrrotmat2vec(ER0) # need to find python version
-    k_hat = np.array([])
+    k_hat = np.array([[temp[0]],[temp[1]],[temp[2]]])
+    theta0 = temp[4]
+    Euldes_lambda = np.zeros((3,len(lambda_)))
+    der_dlambda = np.zeros((1,len(lambda_)))
+    for k in range(len(lambda_)):
+        theta = (1-lambda_[k]) * theta0
+        R = rot(k_hat,theta)
+        Euldes_lambda[:,k] = flip(rotm2eul(R)) # change to work in python\
+        der_dlambda[k] = -theta0
+    
     return
