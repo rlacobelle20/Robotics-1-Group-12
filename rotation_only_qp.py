aa3a56bf8f3c7e9ed969ea13e9efc9763fb800d7
Rachel Lacobelle <rlacobelle02@gmail.com>
2023-11-14 16:22:38 -0500

    added 3 new functions

--- rotation_only_qp.py
+++ rotation_only_qp.py
@@ -5,12 +5,53 @@ import math
 import general_robotics_toolbox as rox
 import quadprog
 
+
+def qprimelimits_full(qlimit,qprev,N,qpmax,qpmin):
+    n = len(qlimit)
+    
+    # compute limits due to joint stops
+    lb_js = N*(qlimit[:,0] - qprev)
+    ub_js = N*(qlimit[:,1] - qprev)
+    
+    # compare and find most restrictice bound
+    lb = np.zeros((n+2,1))
+    ub = np.zeros((n+2,1))
+    ub[-2] = 1
+    ub[-1] = 1
+    
+    for k in range(n):
+        if lb_js[k] > qpmin[k]:
+            lb[k] = lb_js[k]
+        else:
+            lb[k] = qpmin[k]
+            
+        if ub_js[k] < qpmax[k]:
+            ub[k] = ub_js[k]
+        else:
+            ub[k] = qpmax[k]    
+            
+    return lb,ub
+
+# G = H
+def getqp_G_rotationOnly(dq, J, vr, er):
+    n = len(dq)
+    G1 = np.dot( ( np.array(J, np.zeros((3,1))) ).T,np.array( J, np.zeros((3,1)) ) )
+    G2 = np.dot( (np.array([np.zeros((3,n)), vr], [np.zeros((3,n)), np.zeros((3,1))])).T,  np.array([np.zeros((3,n)), vr], [np.zeros((3,n)), np.zeros((3,1))]))
+    G3 = -2 * np.dot( (np.array(J,np.zeros((3,1)))).T, np.array(np.zeros((3,n)), vr) )
+    G3 = (G3 + G3.T) / 2
+    G4 = np.dot( (np.array([np.zeros((1,n)), math.sqrt(er)], [np.zeros((1,n)), 0])).T, np.array([np.zeros((1,n)), math.sqrt(er)], [np.zeros((1,n)), 0]) )
+    G = 2 * (G1+G2+G3+G4)
+    return G
+
+def getqp_f_rotationOnly( dq, er ):
+    f = -2*(np.zeros((1,len(dq),er)).T)
+    return f
+
 """
 translation of matlab code into python
 input: robot, q0, R0Td, epsilon_r, q_prime_min, q_prime_max
 output: q_lambda, lambda, P0T_lambda, R0T_lambda
 """
-
 def qpPathGen_rotationOnly(robot,q0,R0Td,epsilon_r,q_prime_min,q_prime_max,N):
     n = len(q0)
     lambda_ = np.arange(0,1,1/N) # creates an array 0-1 in increments of 1/N
@@ -20,15 +61,16 @@ def qpPathGen_rotationOnly(robot,q0,R0Td,epsilon_r,q_prime_min,q_prime_max,N):
     
     # compute path in task space
     ER0 = np.dot(R0Td,R0Td.T) # ER0 = R0T0*R0Td'
-    temp = vrrotmat2vec(ER0) # need to find python version
+    temp = rox.R2rot(ER0) # need to find python version
     k_hat = np.array([[temp[0]],[temp[1]],[temp[2]]])
     theta0 = temp[4]
     Euldes_lambda = np.zeros((3,len(lambda_)))
     der_dlambda = np.zeros((1,len(lambda_)))
     for k in range(len(lambda_)):
         theta = (1-lambda_[k]) * theta0
-        R = rot(k_hat,theta)
-        Euldes_lambda[:,k] = flip(rotm2eul(R)) # change to work in python\
+        R = rox.rot(k_hat,theta)
+        Euldes_lambda[:,k] = np.flipud(rox.q2R(R)) # this might be very wrong --> should flip matrix
         der_dlambda[k] = -theta0
     
+    # solve qp problem and generate join space path
     return
